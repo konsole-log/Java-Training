15fa789ed32b86340782b0d24455c2edfc4ff6b8
konsole-log <anushshrestha00@gmail.com>
2025-07-05 04:47:33 +0000

    modified

--- classday4/Main.java
+++ classday4/Main.java
@@ -2,7 +2,6 @@ package classday4;
 
 public class Main {
     public static void main(String[] args) {
-        Bank b;
-        System.out.println(b.calculateInterest(0));
+        System.out.println("hello");
     }
 }
